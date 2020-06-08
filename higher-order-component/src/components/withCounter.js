e8aa5c16452ec27ee776e06ae9f21db416270ad5
jdevenish <justin.devenish@gmail.com>
2020-06-08 18:08:22 -0400

    Updating naming convention

--- higher-order-component/src/components/withCounter.js
+++ higher-order-component/src/components/withCounter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const UpdatedComponent = OriginalComponent => {
-    class NewComponent extends React.Component {
+const withCounter = WrappedComponent => {
+    class WithCounter extends React.Component {
         constructor(props) {
             super(props);
 
@@ -17,11 +17,15 @@ const UpdatedComponent = OriginalComponent => {
         };
 
         render() {
-            return <OriginalComponent count={this.state.count} incrementCount={this.incrementCount}/>
+            return <WrappedComponent
+                count={this.state.count}
+                incrementCount={this.incrementCount}
+                {...this.props}
+            />
         }
     }
 
-    return NewComponent
+    return WithCounter
 }
 
-export default UpdatedComponent
+export default withCounter
